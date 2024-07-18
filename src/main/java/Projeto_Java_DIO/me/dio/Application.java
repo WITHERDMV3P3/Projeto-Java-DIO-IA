a3f8a60ed3bedef068f226de9a5fb0977398ad71
Cristian Guedes <cristian.edel@yahoo.com>
2024-07-18 11:25:38 -0300

    Initial commit

---
+++ src/main/java/Projeto_Java_DIO/me/dio/Application.java
@@ -0,0 +1,13 @@
+package Projeto_Java_DIO.me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto_Java_DIO/me/dio/ProjetoJavaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto_Java_DIO.me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
