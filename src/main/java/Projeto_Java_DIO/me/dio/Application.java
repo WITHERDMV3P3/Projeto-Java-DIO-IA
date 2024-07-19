5992257ca8cc0839de7e61689dcf219f4e7a3690
Cristian Guedes <cristian.edel@yahoo.com>
2024-07-19 13:45:23 -0300

    New Commit Com tudo funcionando

--- src/main/java/Projeto_Java_DIO/me/dio/Application.java
+++ src/main/java/Projeto_Java_DIO/me/dio/Application.java
@@ -3,6 +3,7 @@ package Projeto_Java_DIO.me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server (url = "/", descripition = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
